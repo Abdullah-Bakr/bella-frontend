4881fafcf96bdbb441f0e4576e7f722d2becccf0
Abdullah-Bakr <abdullahbakr364@gmail.com>
2024-02-26 20:56:07 +0200

    fix the path

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "https://bella-backend.onrender.com//api";
+const apiUrl = "https://bella-backend.onrender.com/api";
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
